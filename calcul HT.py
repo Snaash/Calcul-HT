4db45249bc2f1f9b781de52273087621636d5004
N0tbad <noe2.kevin@gmail.com>
2022-11-02 21:15:25 +0100

    Calcul HT

--- calcul HT.py
+++ calcul HT.py
@@ -1,10 +1,19 @@
+import os
+
+
+#Le prix est à définir par l'utilisateur dans la variable prix, le calcul HT se fait sur la base 20%
+
 def hors_taxe():
     prix = int(input('Entrez un prix: '))
     hors_taxe = 0.2
 
+#Ici on effectue le calcul du produit HT pour ensuite l'afficher
+
     prix_horstaxe = prix-(prix*hors_taxe)
     print(f"Le prix est de {prix_horstaxe}€ HT")
 
+
+#Dans cette partie, nous allons fermer ou relancer le script en fonction des besoins
 
 hors_taxe()
 while True:
@@ -13,4 +22,4 @@ while True:
         print('Ok on relance')
         hors_taxe()
     elif quit_restart == "Q" or "q":
-        break
+        os.exit(0)
